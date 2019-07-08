c3e10d18b416f3dae1fd7ec0de7f2e15cb8b1981
Andreas Wilm <andreas.wilm@gmail.com>
2019-07-08 05:29:22 +0000

    Enabled auto mpi module loading and made dingo build more flexible

--- specs/default/cluster-init/files/build_dingo_base.sh
+++ specs/default/cluster-init/files/build_dingo_base.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-# See also https://github.com/ICRAR/cloud-dingo/blob/master/azure/Build%20DINGO%20Base%20-%20CentOS7%20Azure.md
+# - See also https://github.com/ICRAR/cloud-dingo/blob/master/azure/Build%20DINGO%20Base%20-%20CentOS7%20Azure.md
+# - Installs to current directory
 
 
-cd
 yum -y install git
-git clone https://bitbucket.csiro.au/scm/askapsdp/yandasoft.git
 yum -y install lapack-devel.x86_64 lapack.x86_64 python2-numpy xerces-c-devel hdf5-devel
-cd yandasoft
+
+git clone https://bitbucket.csiro.au/scm/askapsdp/yandasoft.git
+pushd yandasoft
+git checkout 961adb4
+
 git clone https://github.com/casacore/casacore
-cd casacore/python
+pushd casacore
+git checkout 0f7c684
+pushd python
 # FIXING Python/Cmake hiccup
 sed -i -e 's,GREATER 11,GREATER 15,' CMakeLists.txt
-cd ~/yandasoft/
+popd
+popd
+
+# as root
 ./build_all.sh -s centos -S -c -a -r -y -j 4
 
-cd git clone https://github.com/ICRAR/cloud-dingo.git
-
-
-
-
+popd
+git clone https://github.com/ICRAR/cloud-dingo.git
+cd cloud-dingo
+git checkout 6c9c36e
--- specs/default/cluster-init/scripts/99_install_openmpi.sh
+++ specs/default/cluster-init/scripts/99_install_openmpi.sh
@@ -4,4 +4,5 @@ set -ex
 
 yum install -y openmpi environment-modules
 
+echo "module load mpi" >> ~cycleadmin/.bashrc
  
